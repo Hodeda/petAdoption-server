4704c4faac5bd4305df966d1ff9f251c0a1980a6
Hodeda <orhodeda@gmail.com>
2023-05-27 00:13:07 +0300

    fixed cors url

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://pet-adoption-client-pearl.vercel.app/",
+      "https://pet-adoption-client-pearl.vercel.app",
     ],
   })
 );
